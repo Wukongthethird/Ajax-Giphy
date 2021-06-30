5de12edb20e22567c3c3f865541e4266e2f3fa21
Wukongthethird <simonzhang9518@gmail.com>
2021-06-29 18:09:23 -0700

    finished

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 console.log("Let's get this party started!");
+let gif;
 
-async function getGif(){
-    let newGif = $(".text-input").val()
-    let apiKey = "0jgJnoGdV3pvTQnbzJuF2E3lnLTzVSJb"
-
-    //let gif = await axios.get( "api.giphy.com/v1/gifs/translate", newGif )
-    
-    //let gif = await axios.get( "api.giphy.com/v1/gifs/search" ,  {q:newGif , api_key:"0jgJnoGdV3pvTQnbzJuF2E3lnLTzVSJb" } )
-
-    let gif = await axios.get( `http://api.giphy.com/v1/gifs/search?q=${newGif}&api_key=0jgJnoGdV3pvTQnbzJuF2E3lnLTzVSJb` )
-    
-    
-    return gif.data
+async function getGif() {
+    let gifInput = $(".text-input").val()
+    apiKey = "0jgJnoGdV3pvTQnbzJuF2E3lnLTzVSJb"
+    gif = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${gifInput}&api_key=${apiKey}`)
+    console.log(gif.data);
+    return gif.data.data[0].images.original.url;
 }
 
-$(".search").on("click",getGif) 
+let submitButton = document.querySelector(".addGif");
+submitButton.addEventListener("click", handleClick);
+async function handleClick(evt) {
+    evt.preventDefault();
+    let selectedGif = await getGif();
+    console.log(selectedGif);
+    let newGif = document.createElement("img");
+    newGif.className = "gif";
+    newGif.src = selectedGif;
+    let gifContainer = document.querySelector(".print");
+    gifContainer.append(newGif);
+}
+
+let $remove = $(".remove");
+$remove.on("click", removeAll);
+function removeAll(evt) {
+evt.preventDefault();
+$(".print").empty();
+}
+
+
+
